9e5b3d7bc9756975fbdeb0bd3074cfa28604389b
cmn180003 <81604577+cmn180003@users.noreply.github.com>
2023-02-27 10:32:45 -0600

    add ngrams1

---
+++ ngrams1/ngrams1.py
@@ -0,0 +1,8 @@
+import sys
+
+# Starts program
+if __name__ == '__main__':
+    # Reads system argument
+    if len(sys.argv) < 2:
+        print('Please enter a filename as a system arg')
+        quit()
