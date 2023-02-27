fcaed409662065734ee085e35945fb8c4575ae27
cmn180003 <81604577+cmn180003@users.noreply.github.com>
2023-02-27 16:26:15 -0600

    completed steps up to 1e
    - need to make dictionaries next
    - keep in mind newline thing if it causes problems

--- ngrams1/ngrams1.py
+++ ngrams1/ngrams1.py
@@ -1,4 +1,40 @@
+# Homework 4.1
+# Crystal Ngo and
+
 import sys
+import pathlib
+import nltk
+from nltk import word_tokenize
+import re
+
+
+def makelangmodel(filename):
+    '''
+    Makes language model
+    Args:
+        filename: name of single language file
+    Returns: dict, dict
+        unigram dictionary and bigram dictionary
+    '''
+    # Reads file and removes newlines
+    rel_path = sys.argv[1] + '/' + filename
+    with open(pathlib.Path.cwd().joinpath(rel_path), 'r') as f:
+        text_in = f.read()
+
+    # Makes unigrams list
+    unigrams = word_tokenize(text_in)
+    #print(unigrams)
+
+    # Makes bigrams list
+    bigrams = [(unigrams[k], unigrams[k + 1]) for k in range(len(unigrams) - 1)]
+    #print(bigrams)
+
+    # Makes unigram dictionary
+    uni_dict = {}
+    # Makes bigram dictionary
+    bi_dict = {}
+
+    return uni_dict, bi_dict
 
 # Starts program
 if __name__ == '__main__':
@@ -6,3 +42,11 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Please enter a filename as a system arg')
         quit()
+
+    # Reads data
+    #rel_path = sys.argv[1]
+
+    #Makes language model for each language file
+    eng_uni, eng_bi = makelangmodel('LangId.train.English')
+    #fr_uni, fr_bi = makelangmodel(LangId.train.French)
+    #it_uni, it_bi = makelangmodel(LangId.train.Italian)
