05be7f42b5f1f5ee52a82367f1e4f9d08e6a40de
kiara-aleecia <60724152+kiara-aleecia@users.noreply.github.com>
2023-03-01 18:15:52 -0600

    completed 1f-1i and 2(all)

--- ngrams1/ngrams1.py
+++ ngrams1/ngrams1.py
@@ -1,52 +1,98 @@
 # Homework 4.1
-# Crystal Ngo and
+# Crystal Ngo and Kiara Madeam
 
 import sys
 import pathlib
 import nltk
 from nltk import word_tokenize
 import re
+import pickle
 
+'''
+TO DO (PROGRAM 1):
+[X]- create function w/ filename as argument
+[X]- read in text and remove newlines
+[X]- tokenize text
+[X]- create bigrams list w/ nltk
+[X]- create unigrams list w/ nltk
+[X]- use bigram list to create dictionary of bigrams and counts
+    - [‘token1 token2’] -> count
+[X]- use unigram list to create dictionary of unigrams and counts
+    - [‘token’] -> count
+[X]- return unigram/bigram dictionary from the function
+- IN MAIN:
+    - call function 3 times (english, french, italian)
+    - pickle 6 dictionaries
+'''
 
-def makelangmodel(filename):
-    '''
-    Makes language model
-    Args:
-        filename: name of single language file
-    Returns: dict, dict
-        unigram dictionary and bigram dictionary
-    '''
-    # Reads file and removes newlines
+'''
+Makes language model
+Args:
+    filename: name of single language file
+Returns: dict, dict
+    unigram dictionary and bigram dictionary
+'''
+def make_language_model(filename):
+    
+    # Reads file
     rel_path = sys.argv[1] + '/' + filename
-    with open(pathlib.Path.cwd().joinpath(rel_path), 'r') as f:
+    with open(pathlib.Path.cwd().joinpath(rel_path), 'r', encoding='utf8') as f:
         text_in = f.read()
 
+    # tokenize text
+    tokens = word_tokenize(text_in)
     # Makes unigrams list
-    unigrams = word_tokenize(text_in)
+    unigrams = tokens
     #print(unigrams)
 
     # Makes bigrams list
     bigrams = [(unigrams[k], unigrams[k + 1]) for k in range(len(unigrams) - 1)]
     #print(bigrams)
 
-    # Makes unigram dictionary
-    uni_dict = {}
-    # Makes bigram dictionary
-    bi_dict = {}
+    # Makes unigram dictionary {str:int}
+    uni_dict = {t:unigrams.count(t) for t in set(unigrams)}
+    # Makes bigram dictionary {tuple(str, str):int}
+    bi_dict = {b:bigrams.count(b) for b in set(bigrams)}
 
     return uni_dict, bi_dict
+
+'''
+create unigram and bigram dictionaries for each language and pickles them
+Args:
+    n/a
+Returns:
+    n/a (prints done message)
+'''
+
+def main():
+    # make uni/bi dict for each language
+    eng_uni, eng_bi = make_language_model('LangId.train.English')
+    fr_uni, fr_bi = make_language_model('LangId.train.French')
+    it_uni, it_bi = make_language_model('LangId.train.Italian')
+
+
+    # pickle the uni/bi for all three languages (dict)
+    # english
+    pickle.dump(eng_uni, open('eng_uni.pickle', 'wb'))
+    pickle.dump(eng_bi, open('eng_bi.pickle', 'wb'))
+
+    # french
+    pickle.dump(fr_uni, open('fr_uni.pickle', 'wb'))
+    pickle.dump(fr_bi, open('fr_bi.pickle', 'wb'))
+
+    # italian
+    pickle.dump(it_uni, open('it_uni.pickle', 'wb'))
+    pickle.dump(it_bi, open('it_bi.pickle', 'wb'))
+    
+    print("all done!!")
 
 # Starts program
 if __name__ == '__main__':
     # Reads system argument
     if len(sys.argv) < 2:
-        print('Please enter a filename as a system arg')
+        print("Usage: python3 ngrams1.py data")
         quit()
+     #rel_path = sys.argv[1]
 
-    # Reads data
-    #rel_path = sys.argv[1]
-
-    #Makes language model for each language file
-    eng_uni, eng_bi = makelangmodel('LangId.train.English')
-    #fr_uni, fr_bi = makelangmodel(LangId.train.French)
-    #it_uni, it_bi = makelangmodel(LangId.train.Italian)
+main()
+    
---
+++ ngrams1/ngrams2.py
@@ -0,0 +1,124 @@
+# Homework 4.1
+# Crystal Ngo and Kiara Madeam
+
+import sys
+import pathlib
+import nltk
+from nltk import word_tokenize
+from nltk.util import ngrams
+#import re
+import pickle
+import math
+
+'''
+TO DO:
+[X]- read in pickled dictionaries
+[X]- for each line in test file:
+    [X]- calculate probability for each language
+    [X]- write language with highest probability to a file
+[X]- compute and output accuracy as 'percentage of CORRECTLY CLASSIFIED
+    instances in the test set' (check .sol file)
+[X]- output your accuracy
+[X]- output line numbers of incorrectly classified items
+'''
+
+'''
+calculate probability for line language based on bigrams/unigrams dict
+Args: string, dict{tuple(str, str): int}, dict{str: int}, list[str] 
+    line of file, bi dict {bigram: count}, uni dict {uni: count}, hashset of vocab
+Returns: double
+    probability w/ laplace smoothing
+'''
+def test_language_model(line, bi_dict, uni_dict, V):
+    unigrams_test = word_tokenize(line)
+    bigrams_test = list(ngrams(unigrams_test, 2))
+    
+    p_laplace = 1  # calculate p using Laplace smoothing
+
+    for bigram in bigrams_test:
+        n = bi_dict[bigram] if bigram in bi_dict else 0
+        d = uni_dict[bigram[0]] if bigram[0] in uni_dict else 0
+        p_laplace = p_laplace * ((n + 1) / (d + V))
+    
+    return p_laplace
+    
+'''
+Makes language model
+Args:
+    n/a
+Returns: 
+    n/a (writes to file with language guesses)
+'''
+def main():
+    filename = 'data/LangId.test'
+    # read the pickles back in
+    eng_bi = pickle.load(open('eng_bi.pickle', 'rb'))
+    eng_uni = pickle.load(open('eng_uni.pickle', 'rb'))
+
+    fr_bi = pickle.load(open('fr_bi.pickle', 'rb'))
+    fr_uni = pickle.load(open('fr_uni.pickle', 'rb'))
+
+    it_bi = pickle.load(open('it_bi.pickle', 'rb'))
+    it_uni = pickle.load(open('it_uni.pickle', 'rb'))
+
+    # open file to write guesses and read in test file lines
+    res_file = open('calculate_language.txt', 'w')
+    #rel_path = sys.argv[1] + '/' + filename
+    with open(pathlib.Path.cwd().joinpath(filename), 'r', encoding='utf8') as f:
+        text_in = f.read().splitlines()
+
+    # calculate probability for each language and write to file
+    vocab = eng_uni | fr_uni | it_uni
+    #N = len(vocab)
+    V = len(set(vocab))
+
+    # test probability for each language and write most probable to file
+    for line in text_in:
+        eng_prob = test_language_model(line, eng_bi, eng_uni, V)
+        fr_prob = test_language_model(line, fr_bi, fr_uni, V)
+        it_prob = test_language_model(line, it_bi, it_uni, V)
+        results = max(eng_prob, fr_prob, it_prob)
+
+        if(results == eng_prob):
+            res_file.write('English\n')
+        elif(results == fr_prob):
+            res_file.write('French\n')
+        elif(results == it_prob):
+            res_file.write('Italian\n')
+    res_file.close()
+
+    # read in file that we just created and wrote our results to
+    filename = 'calculate_language.txt'
+    with open(pathlib.Path.cwd().joinpath(filename), 'r', encoding='utf8') as f:
+        results = f.read().splitlines()
+    
+    # check against .sol file and output accuracy and lines missed
+    filename = 'data/LangId.sol'
+    #rel_path = sys.argv[1] + '/' + filename
+    with open(pathlib.Path.cwd().joinpath(filename), 'r', encoding='utf8') as f:
+        text_in = f.read().splitlines()
+    answers = [tuple(line.split()) for line in text_in]
+
+    missed = []
+    for i in range(len(results)):
+        key = answers[i]
+        if(results[i] != key[1]):
+            missed.append(key[0])
+    correct = len(results) - len(missed)
+    accuracy = (correct / len(results)) * 100
+    print(f"Accuracy: {accuracy:.2f}%")
+    for m in missed:
+        print(f"Incorrect on line {m}")
+
+    
+
+if __name__ == '__main__':
+    # Reads system argument
+    if len(sys.argv) != 1:
+        
+        print("Usage: python3 ngrams2.py")
+        quit()
+     
+
+main()
+   
